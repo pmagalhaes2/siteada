d445cf0f14b7a495c12d6c9bb805c58bc59142e4
Patricia Magalhães <paath.magalhaes2@gmail.com>
2021-02-28 01:34:26 -0300

    Finished

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.getElementById("botao-enviar").addEventListener("click", validaformulario)
+
+function validaformulario(){
+  if(document.getElementById("nome").value != "" && 
+  document.getElementById("email").value != "" &&
+  document.getElementById("telefone").value != ""){
+  alert("Prontinho! Você receberá as novidades por e-mail.")
+  }else{
+  alert("Por favor, preencha os campos nome e e-mail!")
+  }
+}
